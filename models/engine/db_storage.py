1e4291284feea87b767f56b8b15a825fdd32b0a0
Dany <danyramirezg@gmail.com>
2019-12-19 16:20:12 -0500

    Add parameter 'Place' to db_storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,7 +40,7 @@ class DBStorage:
     def all(self, cls=None):
         """returns a dictionary
         """
-        classes = ['State', 'City', 'User']
+        classes = ['State', 'City', 'User', 'Place', 'Review']
         objs = {}
 
         if cls is None:
