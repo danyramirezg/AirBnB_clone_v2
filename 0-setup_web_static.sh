f562b9129981b5d469a6860fb0564d8eb1110310
danyramirezg <danyramirezg@gmail.com>
2020-01-03 21:47:42 +0000

    Correct symbolic link order

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 # sets up the web servers for the deployment of web_static
 
 # Install nginx web server:
-# apt-get -y update && apt-get install -y nginx
+sudo apt-get -y update && apt-get install -y nginx
 
 # Create folders if don't exist:
 
@@ -11,17 +11,27 @@ sudo mkdir -p "/data/web_static/shared/"
 sudo mkdir -p "/data/web_static/releases/test/"
 
 # Create a fake HTML file:
-sudo touch /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html 
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-#variable="/data/web_static/current/ $hbnb_static;"
-#sed -i "24i $variable" /etc/nginx/sites-available/default
+
+sed -i "32i location  /hbnb_static/ {" /etc/nginx/sites-available/default
+sed -i "33i alias /data/web_static/current/;" /etc/nginx/sites-available/default
+sed -i "34i autoindex off;" /etc/nginx/sites-available/default
+sed -i "35i }" /etc/nginx/sites-available/default
 
 # Restart nginx:
-service nginx restart
+sudo service nginx restart
