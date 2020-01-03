a9a15fdbb50b4565b9bc9c45ef48aabae1136f46
Dany <danyramirezg@gmail.com>
2020-01-03 15:37:21 -0500

    Remove $ of UBUNTU

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R $ubuntu:$ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 #variable="/data/web_static/current/ $hbnb_static;"
