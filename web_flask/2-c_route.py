d9b1a2a2ec61c7f3dddac5b154d01549041c4d05
Dany <danyramirezg@gmail.com>
2020-01-20 17:30:35 -0500

    Display “C ” followed by the value of the text variable (replace underscore _ symbols with a space)

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+
+"""__name__ means this current file. In this case, it will be main.py.
+ This current file will represent my web application."""
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """Routes: /: display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Routes: /hbnb: display “HBNB" """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """Routes: /c/<text> Display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
