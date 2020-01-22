464b68e70bc08ea5af0d458544b5b451ba4a2a7a
Dany <danyramirezg@gmail.com>
2020-01-21 19:39:48 -0500

    Update state file

--- models/state.py
+++ models/state.py
@@ -2,6 +2,10 @@
 """This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+import os
+import models
+from models.city import City
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -12,5 +16,16 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    # ... for DBStorage: class attribute cities must represent a
-    # relationship with the class City. If the
+    """If environment variable is different to DBStorage return the list of
+    City objects from storage linked to the current State """
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list.append(city)
+            return list
+    else:
+        cities = relationship("City", backref="state", cascade="delete")
